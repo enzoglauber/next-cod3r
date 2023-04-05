62191a2190d1fc5dae2c964cf1541e728fa63060
Enzo Glauber <enzojob@gmail.com>
2023-04-04 21:07:02 -0300

    integrating with ts

--- doors-section-4/model/door.ts
+++ doors-section-4/model/door.ts
@@ -1,10 +1,10 @@
-export default class Door {
-  #number
-  #gift
-  #selected
-  #opened
+export default class DoorModel {
+  #number: number
+  #gift: boolean
+  #selected: boolean
+  #opened: boolean
 
-  constructor(number, gift = false, selected = false, opened = false) {
+  constructor(number: number, gift = false, selected = false, opened = false) {
     this.#number = number
     this.#gift = gift
     this.#selected = selected
@@ -26,16 +26,16 @@ export default class Door {
 
   toggle() {
     const selected = !this.selected
-    return new Door(this.number, this.gift, selected, this.opened)
+    return new DoorModel(this.number, this.gift, selected, this.opened)
   }
 
   open() {
     const opened = true
-    return new Door(this.number, this.gift, this.selected, opened)
+    return new DoorModel(this.number, this.gift, this.selected, opened)
   }
 
   unselect() {
     const selected = false
-    return new Door(this.number, this.gift, selected, this.opened)
+    return new DoorModel(this.number, this.gift, selected, this.opened)
   }
 }
--- doors-section-4/pages/index.jsx
+++ doors-section-4/pages/index.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import Door from '../components/Door'
 import Gift from '../components/Gift'
+import DoorModel from '../model/door'
 
 export default function Home() {
+  const [door1, setDoor1] = useState(new DoorModel(2, false, true))
+
   return (
     <div style={{ display: 'flex' }}>
       <Gift />
-      <Door />
-      <Door selected />
+      <Door door={door1} selected />
     </div>
   )
 }
