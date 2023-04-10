b4f7c5a9018edb1d5afbbe9c181317046c2a1e6a
Enzo Glauber <enzojob@gmail.com>
2023-04-10 20:44:55 -0300

    static #01

---
+++ rendering-section-5/pages/static1.tsx
@@ -0,0 +1,15 @@
+// import { useEffect, useState } from 'react'
+
+export default function Static1() {
+  // const [num, setNum] = useState(0)
+  // useEffect(() => {
+  //   setNum(Math.random())
+  // }, [])
+
+  return (
+    <>
+      <h1>Static #01</h1>
+      {/* <h2>{num}</h2> */}
+    </>
+  )
+}
