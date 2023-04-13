c41607f49ef8b82b554bd560a7ea88560b06a6d5
Enzo Glauber <enzojob@gmail.com>
2023-04-12 21:22:05 -0300

    Question model

---
+++ quiz-section-6/model/answer.ts
@@ -0,0 +1,3 @@
+export default class AnswerModel {
+  #showed: boolean
+}
---
+++ quiz-section-6/model/question.ts
@@ -0,0 +1,39 @@
+import AnswerModel from './answer'
+
+export default class QuestionModel {
+  #id: number
+  #statement: string
+  #answers: AnswerModel[]
+  #right: boolean
+
+  constructor(id: number, statement: string, answers: AnswerModel[], right = false) {
+    this.#id = id
+    this.#statement = statement
+    this.#answers = answers
+    this.#right = right
+  }
+
+  get id() {
+    return this.#id
+  }
+
+  get statement() {
+    return this.#statement
+  }
+
+  get answers() {
+    return this.#answers
+  }
+
+  get right() {
+    return this.#right
+  }
+
+  get notAnswered() {
+    return !this.answered
+  }
+
+  get answered() {
+    return false
+  }
+}
