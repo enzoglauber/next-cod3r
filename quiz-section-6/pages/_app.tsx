e3d57d60de648a4465dcec1b03e1ad82ebdd6cdc
Enzo Glauber <enzojob@gmail.com>
2023-04-15 00:23:11 -0300

    starting quetion component

--- quiz-section-6/pages/_app.tsx
+++ quiz-section-6/pages/_app.tsx
@@ -1,7 +1,17 @@
+import { Poppins } from 'next/font/google'
 import '../styles/globals.css'
 
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '700'],
+  subsets: ['latin-ext']
+})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
