e40a80e3645c86cc9ff40674850a559415903bb9
Enzo Glauber <enzojob@gmail.com>
2023-04-23 11:32:06 -0300

    result component

--- quiz-section-6/pages/result.tsx
+++ quiz-section-6/pages/result.tsx
@@ -1,3 +1,20 @@
+import { useRouter } from 'next/router'
+import styles from '../styles/Result.module.css'
+
 export default function Result() {
-  return <h1>result</h1>
+  const router = useRouter()
+
+  const total = +router.query.total
+  const rights = +router.query.rights
+  const percent = Math.round((rights / total) * 100)
+
+  console.log(total, rights)
+  return (
+    <div className={styles.container}>
+      <h1>Final Result</h1>
+      <div>{total}</div>
+      <div>{rights}</div>
+      <div>{`${percent}%`}</div>
+    </div>
+  )
 }
