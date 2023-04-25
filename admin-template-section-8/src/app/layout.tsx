45a371cb379f31684e5c90e0152acba6101b41e7
Enzo Glauber <enzojob@gmail.com>
2023-04-25 13:18:00 -0300

    font config

--- admin-template-section-8/src/app/layout.tsx
+++ admin-template-section-8/src/app/layout.tsx
@@ -1,4 +1,10 @@
+import { Poppins } from 'next/font/google'
 import './globals.css'
+
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '700'],
+  subsets: ['latin-ext']
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
