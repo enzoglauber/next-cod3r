b930d04d35c1e0c4678be88a06460fd8fcafb7c4
Enzo Glauber <enzojob@gmail.com>
2023-04-25 13:57:04 -0300

    dark mode

--- admin-template-section-8/src/app/components/template/Layout.tsx
+++ admin-template-section-8/src/app/components/template/Layout.tsx
@@ -12,11 +12,16 @@ export default function Layout({ title, subtitle, children }: LayoutProps) {
   return (
     <div
       className={`
-        flex h-screen w-screen
+        flex h-screen w-screen dark
       `}
     >
       <Menu />
-      <div className={`flex flex-col w-full p-7 bg-gray-300`}>
+      <div
+        className={`
+          flex flex-col w-full p-7 bg-gray-300 
+        dark:bg-gray-800 dark:text-gray-50
+        `}
+      >
         <Header title={title} subtitle={subtitle} />
         <Content>{children}</Content>
       </div>
