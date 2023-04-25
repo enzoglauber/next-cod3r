507be9ed033852b753cfadab5e236f25ed80b376
Enzo Glauber <enzojob@gmail.com>
2023-04-25 13:46:32 -0300

    layout component

--- admin-template-section-8/src/app/components/template/Layout.tsx
+++ admin-template-section-8/src/app/components/template/Layout.tsx
@@ -10,10 +10,16 @@ interface LayoutProps {
 
 export default function Layout({ title, subtitle, children }: LayoutProps) {
   return (
-    <div>
+    <div
+      className={`
+        flex h-screen w-screen
+      `}
+    >
       <Menu />
-      <Header title={title} subtitle={subtitle} />
-      <Content>{children}</Content>
+      <div className={`flex flex-col w-full p-7 bg-gray-300`}>
+        <Header title={title} subtitle={subtitle} />
+        <Content>{children}</Content>
+      </div>
     </div>
   )
 }
