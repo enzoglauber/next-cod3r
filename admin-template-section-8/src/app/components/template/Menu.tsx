de78e3be5432850094a8cc2d9bdb475f6f20fb7f
Enzo Glauber <enzojob@gmail.com>
2023-04-26 14:02:58 -0300

    Logo component

--- admin-template-section-8/src/app/components/template/Menu.tsx
+++ admin-template-section-8/src/app/components/template/Menu.tsx
@@ -1,3 +1,4 @@
+import Logo from './Logo'
 import MenuItem from './MenuItem'
 import { AdjustmentsVertical, Bell, Home } from './icons'
 
@@ -6,6 +7,15 @@ interface MenuProps {}
 export default function Menu(props: MenuProps) {
   return (
     <aside>
+      <div
+        className={`
+          flex flex-col items-center justify-center
+          bg-gradient-to-r from-indigo-500 to-purple-800
+          h-20 w-20
+        `}
+      >
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" text="Home" icon={Home} />
         <MenuItem url="/adjustments" text="Adjustments" icon={AdjustmentsVertical} />
