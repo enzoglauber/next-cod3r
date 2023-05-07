f4029fd3952f96a01a2fe0e39ecc70267faf8464
Enzo Glauber <enzojob@gmail.com>
2023-05-07 01:44:25 -0300

    layout and title component

--- next-crud-section-9/src/app/page.tsx
+++ next-crud-section-9/src/app/page.tsx
@@ -1,18 +1,18 @@
+import Layout from '@/components/Layout'
+
 export default function Home() {
   return (
     <div
       className={`
         flex h-screen justify-center items-center
-        bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
+        bg-gradient-to-r from-blue-500 to-purple-500
+        text-white
       `}
     >
-      <span
-        className={`
-          text-4xl
-        `}
-      >
-        Texto
-      </span>
+      <Layout title="Simple Register">
+        <h1>App</h1>
+        <span>content</span>
+      </Layout>
     </div>
   )
 }
---
+++ next-crud-section-9/src/components/Title.tsx
@@ -0,0 +1,22 @@
+interface TitleProps {
+  children?: React.ReactNode
+}
+
+export default function Title({ children }: TitleProps) {
+  return (
+    <div
+      className={`
+        flex flex-col justify-center
+      `}
+    >
+      <h1
+        className={`
+          px-5 py-2 text-2xl
+        `}
+      >
+        {children}
+      </h1>
+      <hr className={`border-2 border-purple-500`} />
+    </div>
+  )
+}
