e80f7356577f473d7b430592b51f25ccedc2c28b
Enzo Glauber <enzojob@gmail.com>
2023-05-08 08:21:40 -0300

    table component #02

--- next-crud-section-9/src/components/Table.tsx
+++ next-crud-section-9/src/components/Table.tsx
@@ -7,24 +7,40 @@ interface TableProps {
 export default function Table({ customers }: TableProps) {
   const header = (
     <tr>
-      <th>Code</th>
-      <th>Name</th>
-      <th>Age</th>
+      <th className={`text-left p-4`}>Code</th>
+      <th className={`text-left p-4`}>Name</th>
+      <th className={`text-left p-4`}>Age</th>
     </tr>
   )
 
   const data = () =>
     customers?.map((customer, index) => (
-      <tr key={index}>
-        <td>{customer.id}</td>
-        <td>{customer.name}</td>
-        <td>{customer.age}</td>
+      <tr
+        key={index}
+        className={`
+          ${index % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}
+        `}
+      >
+        <td className={`text-left p-4`}>{customer.id}</td>
+        <td className={`text-left p-4`}>{customer.name}</td>
+        <td className={`text-left p-4`}>{customer.age}</td>
       </tr>
     ))
 
   return (
-    <table>
-      <thead>{header}</thead>
+    <table
+      className={`
+        w-full rounded-xl overflow-hidden
+      `}
+    >
+      <thead
+        className={`
+          text-gray-100
+          bg-gradient-to-r from-purple-500 to-purple-800
+        `}
+      >
+        {header}
+      </thead>
       <tbody>{data()}</tbody>
     </table>
   )
